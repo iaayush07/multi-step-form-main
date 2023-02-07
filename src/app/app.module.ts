8073dbc85d5a9c347af7c17cb9152690ae344387
Ayush Dhimmar <ayush.dhimmar@1rivet.local>
2023-02-07 18:27:22 +0530

    feat: folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
+import { SelectPlanComponent } from './select-plan/select-plan.component';
+import { AddOnsComponent } from './add-ons/add-ons.component';
+import { SummaryComponent } from './summary/summary.component';
+import { ThankYouComponent } from './thank-you/thank-you.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonalInfoComponent,
+    SelectPlanComponent,
+    AddOnsComponent,
+    SummaryComponent,
+    ThankYouComponent
   ],
   imports: [
     BrowserModule,
