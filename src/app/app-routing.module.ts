a4e01d4ce74397987d91bef908bd13193333b135
Ayush Dhimmar <ayush.dhimmar@1rivet.local>
2023-02-08 11:44:52 +0530

    feat: routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddOnsComponent } from './add-ons/add-ons.component';
 import { PersonalInfoComponent } from './personal-info/personal-info.component';
+import { SelectPlanComponent } from './select-plan/select-plan.component';
+import { SummaryComponent } from './summary/summary.component';
+import { ThankYouComponent } from './thank-you/thank-you.component';
 
 const routes: Routes = [
-  { path: 'personal-info', component: PersonalInfoComponent },
-  { path: '', pathMatch: 'full', redirectTo: 'personal-info' }
+  { 
+    path: 'personal-info', 
+    component: PersonalInfoComponent 
+  },
+  { 
+    path: '', 
+    pathMatch: 'full', 
+    redirectTo: 'personal-info' 
+  },
+  {
+    path:'select-plan',
+    component: SelectPlanComponent
+  },
+  {
+    path:'add-ons',
+    component: AddOnsComponent
+  },
+  {
+    path:'summary',
+    component: SummaryComponent
+  },
+  {
+    path:'thank-you',
+    component: ThankYouComponent
+  }
 ];
 
 @NgModule({
